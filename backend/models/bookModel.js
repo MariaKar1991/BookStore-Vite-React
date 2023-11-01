36f111c4f2a0ca22a8c95c155f9e0b8baa6db239
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 15:22:18 +0200

    Update bookModel.2.js

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,23 +1,29 @@
 import mongoose from "mongoose";
 
+// Define the Mongoose schema for the Book model
 const bookSchema = mongoose.Schema(
   {
+    // Title of the book (String), required field
     title: {
       type: String,
       required: true,
     },
+    // Author of the book (String), required field
     author: {
       type: String,
       required: true,
     },
+    // Year the book was published (Number), required field
     publishYear: {
       type: Number,
       required: true,
     },
   },
   {
+    // Add timestamps for createdAt and updatedAt fields
     timestamps: true,
   }
 );
 
-export const Book = mongoose.model("BookStore", bookSchema);
+// Create the Mongoose model named "Book" based on the bookSchema
+export const Book = mongoose.model("Book", bookSchema);
