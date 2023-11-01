9632b6b436fcf28e34965fb88f35d51cd66332f9
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 15:40:32 +0200

    Update BooksCard.2.jsx

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -7,9 +7,11 @@ import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
 import BookSingleCard from "./BookSingleCard";
 
+// BooksCard component displaying a grid of books in card format
 const BooksCard = ({ books }) => {
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {/* Mapping through the list of books and rendering BookSingleCard for each */}
       {books.map((item) => (
         <BookSingleCard key={item._id} book={item} />
       ))}
