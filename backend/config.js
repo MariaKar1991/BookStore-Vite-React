b2ca2cbc15387fccdf77f1df636ffbe1e39a5cc4
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 15:43:52 +0200

    Update config.2.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,1 @@
-export const PORT = 5555;
-
-export const mongoDBURL =
-  "mongodb+srv://mariaskaravazaki:4u3zjerUfz18sQuX@cluster0.420sy19.mongodb.net/Bookstore-Mern?retryWrites=true&w=majority";
+Delete for security purposes. 
