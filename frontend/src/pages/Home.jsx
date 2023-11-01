c4bf47285b4d6f037d0c40925e09663f5ebfdee3
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 15:32:41 +0200

    Update Home.2.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,26 +8,37 @@ import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
 import BooksTable from "../components/home/BooksTable";
 import BooksCard from "../components/home/BooksCard";
 
+// Home component for displaying the list of books
 const Home = () => {
+  // State variables for books data, loading spinner, and display type (table or card)
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
   const [showType, setShowType] = useState("table");
 
+  // useEffect hook to fetch books data from the server when the component mounts
   useEffect(() => {
+    // Set loading to true during the API request
     setLoading(true);
+
+    // Make a GET request to retrieve the list of books
     axios
       .get("http://localhost:5555/books")
       .then((response) => {
+        // Set the books state with the retrieved data and set loading to false
         setBooks(response.data.data);
         setLoading(false);
       })
       .catch((error) => {
+        // Log the error and set loading to false if an error occurs
         console.log(error);
         setLoading(false);
       });
   }, []);
+
+  // JSX structure for the Home component
   return (
     <div className="p-4">
+      {/* Display buttons to switch between table and card view */}
       <div className="flex justify-center items-center gap-x-4">
         <button
           className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
@@ -42,12 +53,16 @@ const Home = () => {
           Card
         </button>
       </div>
+
+      {/* Display heading and add book button */}
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Books List</h1>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
       </div>
+
+      {/* Display loading spinner or BooksTable/BooksCard based on showType */}
       {loading ? (
         <Spinner />
       ) : showType === "table" ? (
