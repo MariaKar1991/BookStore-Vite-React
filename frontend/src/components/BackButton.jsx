15a800d3d5b4273877f8c4f71580d7ab52627336
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 15:34:42 +0200

    Update BackButton.2.jsx

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import { BsArrowLeft } from "react-icons/bs";
 
+// BackButton component for creating a back navigation button
 const BackButton = ({ destination = "/" }) => {
   return (
     <div className="flex">
+      {/* Link component for navigation with a back arrow icon */}
       <Link
         to={destination}
         className="bg-sky-800 text-white px-4 py-1 rounded-lg w-fit"
