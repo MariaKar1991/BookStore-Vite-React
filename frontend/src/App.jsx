0d43e782b1cbfbea03978a196381285254a22eae
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 15:25:46 +0200

    Update App.2.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,23 @@ import ShowBook from "./pages/ShowBook";
 import EditBook from "./pages/EditBook";
 import DeleteBook from "./pages/DeleteBook";
 
+// Main App component defining the application routes
 const App = () => {
   return (
     <Routes>
+      {/* Route for the home page */}
       <Route path="/" element={<Home />} />
+
+      {/* Route for creating books */}
       <Route path="/books/create" element={<CreateBooks />} />
+
+      {/* Route for displaying book details */}
       <Route path="/books/details/:id" element={<ShowBook />} />
+
+      {/* Route for editing book information */}
       <Route path="/books/edit/:id" element={<EditBook />} />
+
+      {/* Route for deleting a book */}
       <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
   );
