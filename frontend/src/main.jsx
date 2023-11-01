1020e9607efb3588193aa2a0217a2587c6e75c98
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 15:27:06 +0200

    Update main.2.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,13 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
 
+// Using ReactDOM.createRoot to render the app
 ReactDOM.createRoot(document.getElementById("root")).render(
+  // Wrapping the entire application with BrowserRouter for routing
   <BrowserRouter>
+    {/* Providing Snackbar notifications using SnackbarProvider */}
     <SnackbarProvider>
+      {/* Rendering the main App component */}
       <App />
     </SnackbarProvider>
   </BrowserRouter>
